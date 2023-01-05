f08e648cb0dbd7ab9bd332a79a8fd701d35df607
achtaitaipai <charles.cailleteau@gmail.com>
2023-01-05 18:19:35 +0100

    replace 'les actualités' by 'notre actu'

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Les actualités";
+export const SITE_TITLE = "Notre Actu";
 export const SITE_DESCRIPTION = "Les actualités du magazine À Fond";
